fa60f44517a1a61e97530fd171178d31fafda030
Vansh Kalra <vansh@erino.io>
2025-11-20 16:46:05 +0530

    Error handling in rust

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,16 @@
-enum Shape {
-    Circle(f64),
-    Square(f64),
-    Rectangle(f64, f64),
-}
+use std::fs;
 
-fn main() {
-    let circle = Shape::Circle(4.0);
-    let square = Shape::Square(5.0);
-    let rect = Shape::Rectangle(2.4, 6.8);
+fn main () {
+    let res = fs::read_to_string("example.txt");
 
-    
-}
+    match res {
+        Ok(string) => {
+            println!("READING: {}", string)
+        },
+        Err(error) => {
+            println!("ERROR found: {}", error)
+        }
+    }
 
-fn calculate_area(shape: Shape) -> f64 {
-    let ans = match shape {
-        Shape::Circle(radius) => 3.14 * radius * radius,
-        Shape::Square(side) => side * side,
-        Shape::Rectangle(width, height ) => width * height
-    };
-    return ans;
+    print!("works")
 }
