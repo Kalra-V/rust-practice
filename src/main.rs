b1bc80bef9646a1ddc3d9dd5944c1b1f6c1ca786
Vansh Kalra <vansh@erino.io>
2025-11-20 14:11:33 +0530

    Enums - Pattern matching

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
+enum Shape {
+    Circle(f64),
+    Square(f64),
+    Rectangle(f64, f64),
 }
 
 fn main() {
-    let user1 = User {
-        active: true,
-        username: String::from("Kin"),
-        email: String::from("vanshkalra645@gmail.com"),
-        sign_in_count: 13321312,
-    };
+    let circle = Shape::Circle(4.0);
+    let square = Shape::Square(5.0);
+    let rect = Shape::Rectangle(2.4, 6.8);
 
-    println!("{}'s email is {}", user1.username, user1.email);
+    
+}
+
+fn calculate_area(shape: Shape) -> f64 {
+    let ans = match shape {
+        Shape::Circle(radius) => 3.14 * radius * radius,
+        Shape::Square(side) => side * side,
+        Shape::Rectangle(width, height ) => width * height
+    };
+    return ans;
 }
